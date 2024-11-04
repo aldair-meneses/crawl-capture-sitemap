6365b20ffdd5a91edf7cb1a12a1f825e87aba787
Aldair Meneses <aldair.meneses@protonmail.com>
2024-11-03 23:05:07 -0300

    add suport to set cookies before crawl; move out crawlerSettings from src directory

--- src/browser.js
+++ src/browser.js
@@ -2,8 +2,9 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 const settings = require('./settings.js');
 const pagesUrls = require('./navigation/pagesManager.js');
+const { timeout } = require('puppeteer');
 
-const { launcherOptions, gotoOptions } = settings;
+const { launcherOptions, gotoOptions, cookies } = settings;
 
 const screenshotDir  = path.join(__dirname, '../screenshots');
 
@@ -12,26 +13,38 @@ exports.crawler = {
         const browser = await puppeteer.launch(
             launcherOptions,
         );
-    
-        console.log('pagesUrl', pagesUrls);
 
-        await Promise.all(pagesUrls.map(async (url) => { 
+        let usingCookies = false;
+        
+        console.table(pagesUrls);
+        
+        for ( url of pagesUrls) {
             const page = await browser.newPage();
+            console.table([{ "Página": url }]);
+                        
+            if (cookies.length > 0 && !usingCookies) {
+                const domain = "https://" + new URL(url).hostname;
+                await page.setCookie(...cookies);
+                await page.goto(domain, gotoOptions );
+                await page.reload();
+                usingCookies = true;
+            }
+
             await page.goto(url, gotoOptions );
             
             const slug  = await page.evaluate(() => {
                 if (window.location.pathname === '/') {
                     return window.location.hostname;
                 }
-                return window.location.pathname;
+                return window.location.pathname.replaceAll('/', '-');
             });
 
             const bodyWidth = await page.evaluate(() => document.body.scrollWidth);
             const bodyHeight = await page.evaluate(() => document.body.scrollHeight);
             await page.setViewport({ width: bodyWidth, height: bodyHeight });
-
+            
             await page.screenshot({ path: `${screenshotDir}/${slug}.png` }, { fullPage: true });
-         }));
+        }
 
         browser.close();
     }
--- src/settings.js
+++ src/settings.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-const crawlerSettings = JSON.parse(fs.readFileSync( path.join(__dirname, '../crawlerSettings.json')));
+const crawlerSettings = JSON.parse(fs.readFileSync( path.join(__dirname, '../crawlerSettings.json')))
 
 const settings = {
     gotoOptions:  crawlerSettings.gotoOptions,
-    launcherOptions: crawlerSettings.launcherOptions
+    launcherOptions: crawlerSettings.launcherOptions,
+    cookies: crawlerSettings.cookies
 }
 
 module.exports = settings;
