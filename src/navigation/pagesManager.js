0e37591c547954e8e287f1f5a240b46e0c9eaa91
Aldair Meneses <aldair.meneses@protonmail.com>
2024-11-03 23:03:10 -0300

    Remove unused filtering of pages option from command line arguments

--- src/navigation/pagesManager.js
+++ src/navigation/pagesManager.js
@@ -3,7 +3,6 @@ const pagesUrls = [];
 const args = process.argv.slice(2);
 
 const options  = args.filter(arg => arg.startsWith('--'));
-const filteredOptions = options.filter(arg => !arg.startsWith('--pages='));
 
 const pagesOption = options.find(arg => arg.startsWith('--pages='));
 if (pagesOption) {
